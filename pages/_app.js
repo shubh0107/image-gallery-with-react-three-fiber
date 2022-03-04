94f9f0254622cd81225745f0730ff2fd7ee350af
Shubham Singh <shubham.singh1@spinny.com>
2022-03-04 11:34:37 +0530

    Added links and styled them

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app-container">
+      <a href="https://www.instagram.com/riaachakravarty/" target="_blank" rel="noreferrer noopener" id="name">
+        Ria Chakravarty
+      </a>
+      <Component {...pageProps} />
+      <a
+        href="https://www.instagram.com/tilldeathwedoart2020/"
+        target="_blank"
+        rel="noreferrer noopener"
+        id="instagram-link"
+      >
+        Follow me
+        <img src="/instagram.svg" alt="instagram_link" />
+      </a>
+    </div>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { Suspense, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Preload, Image as ImageImpl } from "@react-three/drei";
-import { ScrollControls, Scroll, useScroll } from "./ScrollControls";
+import { ScrollControls, Scroll, useScroll } from "../lib/ScrollControls";
 
 function Image(props) {
   const ref = useRef();
